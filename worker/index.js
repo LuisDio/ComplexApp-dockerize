4390d7b1b83e8af33e072c1ed9a0bab6b32b4153
LuisDio <luis.diolindo@gmail.com>
2019-12-11 00:46:01 +0100

    worker impl init

---
+++ worker/index.js
@@ -0,0 +1,23 @@
+const keys = require('./keys');
+const redis = require('redis');
+
+const redisClient = redis.createClient({
+  host: keys.redisHost,
+  port: keys.redisPort,
+  retry_strategy: () => 1000
+});
+
+const sub = redisClient.duplicate();
+
+function fib(index) {
+  if (index < 2) return 1;
+  return fib(index -1) + fib(index -2);
+}
+
+// subscribe to watch new message(value)
+sub.on('message', (channel, message) => {
+  redisClient.hset('values', message, fib(parseInt(message)));
+});
+
+// on new value inserted to redis
+sub.subscribe('insert');
